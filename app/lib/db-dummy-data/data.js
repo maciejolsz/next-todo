ed902e658f52b67a65d3e63207889cdf8d65016d
maciejolsz <maciejolsz@gmail.com>
2024-02-02 14:04:47 +0100

    - add env config for vercel/postgres - install required bundles - create and populate database with some sample data

---
+++ app/lib/db-dummy-data/data.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a6442a',
+        name: 'MiniMe',
+        email: 'minime@hotmail.com',
+        password: '123456',
+    },
+];
+
+const todos = [
+    {
+        id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+        name: 'Read Atomic habits',
+        details: 'I need to improve my habits step by step. Heard some good opinions!',
+        status: 'new',
+    }
+]
+
+
+module.exports = {
+    users,
+    todos,
+};
