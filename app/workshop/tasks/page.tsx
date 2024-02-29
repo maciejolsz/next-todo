de77c8f7302a71344f536a14120ce9666417316b
maciejolsz <maciejolsz@gmail.com>
2024-02-29 17:11:15 +0100

    - define some roadmap

--- app/workshop/tasks/page.tsx
+++ app/workshop/tasks/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
+
 import AddTask from "@/app/ui/workshop/tasks/add-task";
 import Title from "@/app/ui/title";
 import TasksManagerSkeleton from "@/app/ui/skeletons/tasks-manager";
 import TasksManagerDnd from "@/app/ui/workshop/tasks/tasks-manager-dnd";
-import {fetchTasks} from "@/app/lib/fetch-data";
+import { fetchTasks } from "@/app/lib/fetch-data";
 
 export default async function Page() {
   const tasks = await fetchTasks();
