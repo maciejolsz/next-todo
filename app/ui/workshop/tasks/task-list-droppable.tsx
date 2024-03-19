6992abd3655d5b8c95f1fe99a0122eb8f46bf018
maciejolsz <maciejolsz@gmail.com>
2024-03-19 13:12:45 +0100

    - redesign tasks grid

--- app/ui/workshop/tasks/task-list-droppable.tsx
+++ app/ui/workshop/tasks/task-list-droppable.tsx
@@ -3,12 +3,16 @@ import TaskList from "@/app/ui/workshop/tasks/task-list";
 import { TaskStatusType, TaskType } from "@/app/lib/types";
 
 export default function TaskListSection({type, tasks}: {type: TaskStatusType, tasks: TaskType[]}) {
-  const doubleClass = ["new", "done"].includes(type) ? styles.doubleRow : "";
-  const doneClass = type === "done" ? styles.doneCol : "";
+  const doubleRowSections = ["next"];
+  const doubleColSections = ["on-it"];
+
+  const doubleRowClass = doubleRowSections.includes(type) ? styles.doubleRow : "";
+  const doubleColClass = doubleColSections.includes(type) ? styles.doubleCol : "";
+
   const onItClass = type === "on-it" ? styles.onItClass : "";
+  const nextClass = type === "next" ? styles.nextClass : "";
 
-
-  return <div key={type} className={`${styles.tasksSection} ${doubleClass} ${doneClass} ${onItClass}`}>
+  return <div key={type} className={`${styles.tasksSection} ${doubleColClass} ${onItClass} ${nextClass} ${doubleRowClass}`}>
       <TaskList type={type} tasks={tasks.filter((task) => task.status === type)}/>
     </div>
 }
