c9f3dc2083e40e67de20247733f63e0bfff06378
maciejolsz <maciejolsz@gmail.com>
2024-03-21 16:55:49 +0100

    - redesign empty task section

--- app/ui/workshop/tasks/task-list-empty.tsx
+++ app/ui/workshop/tasks/task-list-empty.tsx
@@ -1,5 +1,6 @@
 import { TaskStatusType } from "@/app/lib/types";
 import TaskListHeader from "@/app/ui/workshop/tasks/task-list-header";
+import {TbDragDrop} from "react-icons/tb";
 
 type TaskListProps = { type: TaskStatusType, isOver: boolean, customRef: (element: HTMLElement | null) => void; }
 
@@ -9,6 +10,7 @@ export default function TaskListEmpty({ type, isOver, customRef }: TaskListProps
   return <div ref={customRef} className={`w-full h-full transition-all duration-100 ease-linear ${isOverClass}`}>
     <TaskListHeader type={type} />
 
-    <div>Nothing here yet...</div>
+    <TbDragDrop className={"block m-auto mt-28"} size={48} color={"rgb(209 213 219)"}/>
+    <div className={"mt-4 text-center text-gray-300"}>Drop something HERE!</div>
   </div>
 }
