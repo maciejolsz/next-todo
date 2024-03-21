4fc34886169eca4107cce1882fe5e1334158a6db
maciejolsz <maciejolsz@gmail.com>
2024-03-21 15:25:25 +0100

    - looking for build bugfixes...

--- app/[locale]/workshop/loading.tsx
+++ app/[locale]/workshop/loading.tsx
@@ -1,7 +1,10 @@
+import { useTranslations } from "next-intl";
+
 import Title from "@/app/ui/title";
 
 export default function Loading() {
+  const t = useTranslations();
   return <div className={"text-center animate-bounce"}>
-    <Title tier={"h3"}>Loading...</Title>
+    <Title tier={"h3"}>{t("Index.loading")}</Title>
   </div>
 }
